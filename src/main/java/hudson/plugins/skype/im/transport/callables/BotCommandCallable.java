a0cda4fc4f2e9570f7758112b33bbc2d9adfadaa
Jarle Hjortland <jarlebh@gmail.com>
2011-05-11 00:05:09 +0200

    Fixed bot commands by using a closure back to master

---
+++ src/main/java/hudson/plugins/skype/im/transport/callables/BotCommandCallable.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hudson.plugins.skype.im.transport.callables;
+
+import com.skype.Chat;
+import com.skype.ChatMessage;
+import com.skype.SkypeException;
+import hudson.plugins.im.IMException;
+import hudson.plugins.im.IMMessage;
+import hudson.plugins.im.IMMessageListener;
+import hudson.plugins.im.bot.Bot;
+import hudson.plugins.skype.im.transport.SkypeChat;
+import hudson.plugins.skype.im.transport.SkypeIMException;
+import hudson.remoting.Callable;
+import hudson.remoting.Channel;
+import java.io.IOException;
+
+/**
+ *
+ * @author jbh
+ */
+public class BotCommandCallable implements Callable<Boolean, SkypeIMException> {
+    
+    Chat chat = null;    
+    String senderId = null;
+    String toId = null;
+    String content = null;
+    public BotCommandCallable(Chat chat, ChatMessage msg) throws SkypeIMException {
+        this.chat = chat;
+        try {
+            senderId = msg.getSenderId();
+            toId = msg.getId();
+            content = msg.getContent();
+        } catch (SkypeException e) {
+            throw new SkypeIMException(e);
+        }
+    }
+
+    public Boolean call() throws SkypeIMException {
+        SkypeChat skypeChat = new SkypeChat(chat) {
+
+            @Override
+            public void sendMessage(String msg) throws IMException {
+                try {
+                    SkypeChatCallable sender = new SkypeChatCallable(new String[]{senderId}, msg);
+                    Channel.current().call(sender);                    
+                    
+                } catch (Exception ex) {
+                    throw new IMException(ex);
+                }
+            }
+            @Override
+             public void addMessageListener(IMMessageListener listener) {
+                 
+             }
+        };
+        Bot bot = new Bot(skypeChat, "hudson",
+                "hostname", "!", null);
+
+
+        if (content != null) {
+            // replay original message:
+
+            bot.onMessage(new IMMessage(senderId, toId, content));//Ask skype
+
+        }
+        return Boolean.TRUE;
+    }
+}
